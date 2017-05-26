bd3969597139827e9a6f26bc282a5792621516b0
Joel Hoover <joelahoover@gmail.com>
2017-05-25 22:29:05 -0400

    Added 'forever' mode to run_local script

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+#
+# Run this shell script from the root of the project
+#
 
 # Setup local variables
 export NMV_DEMO_RESOURCE_SERVER="http://localhost:8010/"
 
 # Start the server
-npm start
+if [ $1 = "forever" ]; then
+	echo "Running forever"
+	forever -w --watchDirectory="$PWD" server.js
+else
+	npm start
+fi
